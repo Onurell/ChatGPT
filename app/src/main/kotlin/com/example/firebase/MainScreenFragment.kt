cd9c87ce11ab143537c6268f57e071e9c13d8713
ahmetonur.argenc <ahmetonur.argenc@dgpaysit.com>
2024-02-14 17:33:37 +0300

    Navigation yapısı bozuk

---
+++ app/src/main/kotlin/com/example/firebase/MainScreenFragment.kt
@@ -0,0 +1,30 @@
+package com.example.firebase
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.cardview.widget.CardView
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+
+class MainScreenFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main_screen, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val chatgpt = view.findViewById<CardView>(R.id.chatgpt)
+
+        chatgpt.setOnClickListener {
+            findNavController().navigate(R.id.action_mainScreenFragment_to_chatgptFragment)
+        }
+    }
+}
